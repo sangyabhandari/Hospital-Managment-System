105858eeba4bb1fb7e02a4e48ecfc637d9b31568
Sangya Bhandari <68247165+sangyabhandari@users.noreply.github.com>
2022-08-14 20:57:30 +0530

    Add files via upload

---
+++ Hospital Management System/src/project/conectionProvider.java
@@ -0,0 +1,30 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+import java.sql.*;
+
+/**
+ *
+ * @author sandi
+ */
+public class conectionProvider {
+    public static Connection getcon()
+    {
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/project","root","123");
+            return con;
+        }
+        catch(Exception e)
+        {
+            return null;
+        }
+    }
+        
+    
+}
